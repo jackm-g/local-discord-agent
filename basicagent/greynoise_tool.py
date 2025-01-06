36c85b7d78e9bf1e0c7d3e7c166d33d70153cdcb
jackm-g <jgrlm@proton.me>
2025-01-05 20:37:01 -0500

    update greynoise tool

--- basicagent/greynoise_tool.py
+++ basicagent/greynoise_tool.py
@@ -1,8 +1,33 @@
 import requests
 from langchain.tools import tool
+from greynoise import GreyNoise
+from greynoise.exceptions import RequestFailure
+import os
+from dotenv import load_dotenv
+import json
 
+# Docs for API: https://greynoise.readthedocs.io/en/latest/index.html
 
-def get_greynoise_data(ip_address: str):
+load_dotenv()
+API_KEY = os.getenv("GREYNOISE_API_KEY")
+
+def get_ip_info(ip_address):
+    """
+    Retrieve information about an IP address from the GreyNoise API.
+    """
+    try:
+        api_client = GreyNoise(api_key=API_KEY)
+        response = api_client.ip(ip_address)
+        print(response)
+        json_data = json.dumps(response)
+        return json_data
+    
+    # TODO: RateLimitError and NotFound Error from greynoise package
+    except RequestFailure as e:
+        print(f"Failed to retrieve data for IP {ip_address}: {e}")
+        return None
+
+def get_ip_greynoise_community(ip_address: str):
     url = f"https://api.greynoise.io/v3/community/{ip_address}"
     response = requests.get(url)
     print("greynoise response")
@@ -12,7 +37,8 @@ def get_greynoise_data(ip_address: str):
 @tool
 def greynoise_ip_address_tool(ip_address: str):
     """Get the greynoise data for the given ip address"""
-    return get_greynoise_data(ip_address)
+    #TODO: Switch once API key is purchased.
+    return get_ip_greynoise_community(ip_address)
 
 if __name__ == "__main__":
-    print(get_greynoise_data("8.8.8.8"))
+    print(get_ip_info("8.8.8.8"))
